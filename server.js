bebe6ed846496aba57fead943b7b5ba58a1b6932
PRAKASH <prakash@PRAKASHs-MacBook-Pro.local>
2025-04-18 00:43:40 +0530

    server - 4 changes

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Serve the home page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "main.html"));
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
